e572c38364fb769d13736bd7423194c10a0fbf3e
MiwaDenverMaomi <dajia.hao.hanabi.0815@gmail.com>
2022-10-05 14:39:31 -0700

    study reading_writing practice

--- 3-2/index.php
+++ 3-2/index.php
@@ -1,6 +1,6 @@
 <?php
 $fruits=array("リンゴ"=>300,"みかん"=>150,"桃"=>3000);
-
+// $fruits=["リンゴ"=>300,"みかん"=>150,"桃"=>3000];
 foreach($fruits as $key=>$val){
   echo "{$key}は{$val}円です。";
   echo "<br/>";
