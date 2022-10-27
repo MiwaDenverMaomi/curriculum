1cdee20c71d4d10e91bdaa5b24dec1a552d4d674
miwa-murata <miwamurata0808@gmail.com>
2022-10-26 23:17:11 -0700

    fizz-buzz fixed

--- 3-1/index.php
+++ 3-1/index.php
@@ -4,9 +4,9 @@ $str="";
 $br="<br>";
 
 for($i=1;$i<=$num;$i++){
-  if($i%3===0){
+  if($i%3===0&&$i%5!==0){
     $str= "Fizz!";
-  }else if($i%5===0){
+  }else if($i%3!==0&&$i%5===0){
     $str=  "Buzz!";
   }else if($i%3===0&&$i%5===0){
     $str=  "FizzBuzz!";
