b02589df87b3c83d1585cce6ab4cef5057958152
miwa-murata <miwamurata0808@gmail.com>
2022-10-26 23:51:00 -0700

    コード修正

--- 3-2/index.php
+++ 3-2/index.php
@@ -1,8 +1,12 @@
 <?php
-$fruits=array("リンゴ"=>300,"みかん"=>150,"桃"=>3000);
+// $fruits=array("リンゴ"=>300,"みかん"=>150,"桃"=>3000);
+$fruits=array("りんご"=>[300,1],"みかん"=>[150,1],"桃"=>[3000,1]);
 
+function calculate($price,$num){
+  return $price*$num;
+}
 foreach($fruits as $key=>$val){
-  echo "{$key}は{$val}円です。";
+  echo "{$key}は".calculate($val[0],$val[1])."円です。";
   echo "<br/>";
 }
 ?>
