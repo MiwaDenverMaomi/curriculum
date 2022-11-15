c7d19f129cb606d3eaf5a5603f3e16d1653e7167
miwa-murata <miwamurata0808@gmail.com>
2022-11-15 07:58:53 -0800

    modification assignment 3-2

--- 3-2/index.php
+++ 3-2/index.php
@@ -1,12 +1,28 @@
 <?php
-// $fruits=array("リンゴ"=>300,"みかん"=>150,"桃"=>3000);
-$fruits=array("りんご"=>[300,1],"みかん"=>[150,1],"桃"=>[3000,1]);
+//今回の提出回答
+$fruits=array("リンゴ"=>300,"みかん"=>150,"桃"=>3000);//フルーツと単価の連想配列
+$q=array(1,1,1);//個数の配列
+$i=0;
 
 function calculate($price,$num){
   return $price*$num;
 }
+
 foreach($fruits as $key=>$val){
-  echo "{$key}は".calculate($val[0],$val[1])."円です。";
+    echo "{$key}は".calculate($val,$q[$i])."円です。";
   echo "<br/>";
+  $i++;
 }
+
+//前回提出回答
+// $fruits=array("りんご"=>[300,1],"みかん"=>[150,1],"桃"=>[3000,1]);
+
+// function calculate($price,$num){
+//   return $price*$num;
+// }
+// foreach($fruits as $key=>$val){
+//   echo "{$key}は".calculate($val[0],$val[1])."円です。";
+//   echo "<br/>";
+// }
+
 ?>
